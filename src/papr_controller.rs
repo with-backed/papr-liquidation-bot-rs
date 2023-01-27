4771789efbb390501a17642991380f1c17b37cb8
Wilson Cusack <cusackw@gmail.com>
2023-01-26 22:02:01 -0700

    alt approach

--- src/papr_controller.rs
+++ src/papr_controller.rs
@@ -1,40 +1,40 @@
 use std::{env, sync::Arc};
-use ethers::{types::{U256, Address}, prelude::abigen, providers::{Http, Provider}, middleware::SignerMiddleware, signers::{LocalWallet, Signer, Wallet} };
+use ethers::{core::k256::ecdsa::SigningKey, types::{U256, Address}, prelude::abigen, providers::{Http, Provider}, middleware::SignerMiddleware, signers::{LocalWallet, Signer, Wallet} };
 
 abigen!(PaprController, "src/abis/PaprController.json");
 
-// pub struct PaprControllerProvider {
-//     provider: SignerMiddleware<ethers::providers::Provider<_>, Wallet<ethers::core::k256::ecdsa::SigningKey>>,
-// }
+pub struct PaprControllerProvider {
+    controller: PaprController<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>
+}
 
-// impl Default for PaprControllerProvider {
-//     fn default() -> Self {
-//         let provider = Provider::<Http>::try_from(
-//             env::var("JSON_RPC_PROVIDER").expect("JSON_RPC_PROVIDER not set"),
-//         ).expect("err");
-//         let chain_id = U256::from_dec_str(&env::var("CHAIN_ID").expect("CHAIN_ID not set")).unwrap();
-//         // this wallet's private key
-//         let wallet = env::var("PRIVATE_KEY").expect("PRIVATE_KEY not set")
-//             .parse::<LocalWallet>()
-//             .unwrap()
-//             .with_chain_id(chain_id.as_u64());
+impl PaprControllerProvider {
+    fn new(controller_addr_str: &str) -> Self {
+        let provider = Provider::<Http>::try_from(
+            env::var("JSON_RPC_PROVIDER").expect("JSON_RPC_PROVIDER not set"),
+        ).expect("err");
+        let chain_id = U256::from_dec_str(&env::var("CHAIN_ID").expect("CHAIN_ID not set")).unwrap();
+        // this wallet's private key
+        let wallet = env::var("PRIVATE_KEY").expect("PRIVATE_KEY not set")
+            .parse::<LocalWallet>()
+            .unwrap()
+            .with_chain_id(chain_id.as_u64());
 
-//         Self {
-//             provider: SignerMiddleware::new(provider, wallet)
-//         }
-//     }
-// }
+        let signer = SignerMiddleware::new(provider, wallet);
+        let controller_addr = controller_addr_str.parse::<Address>().unwrap();
 
-// impl PaprControllerProvider {
-    // async fn newTarget(&self, controller_addr_str: &str) -> Result<U256, eyre::Error>  {
-    //     let controller_addr = controller_addr_str.parse::<Address>().unwrap();
-    //     let controller = PaprController::new(controller_addr, Arc::new(self.provider.clone()));
-    //     Ok(controller.new_target().call().await?)
-    // }
-// }
+        Self {
+            controller: PaprController::new(controller_addr, Arc::new(signer))
+        }
+    }
+
+    async fn newTarget(&self) -> Result<U256, eyre::Error>  {
+        Ok(self.controller.new_target().call().await?)
+    }
+}
+
 
 lazy_static! {
-    static ref PROVIDER : Arc<SignerMiddleware<Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>> = {
+    static ref PROVIDER : Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> = {
         let p = Arc::new({
             // connect to the network
             let provider = Provider::<Http>::try_from(
